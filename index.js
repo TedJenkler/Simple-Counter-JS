5ccc5c8c8f29fe3bf71b85f93925684087f7ad01
Teodor Jenkler <108183860+TedJenkler@users.noreply.github.com>
2022-09-11 13:53:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-// 
 
+let saveEl =  document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
 let count = 0;
 
 const increment = () => {
-    count = count + 1;
-    document.getElementById("count-el").innerText = count;
+    count += 1;
+    countEl.innerHTML = count;
+}
+
+let save = () => {
+    let countSTR = count + " - ";
+    saveEl.textContent += countSTR;
+    countEl.innerHTML = 0;
+    count = 0;
 }
